ea966383d3c44337c36801467c3050ab6a3a2338
Josh Freedman <josh.freedman06@gmail.com>
2026-05-05 16:29:00 -0400

    Found and replaced examplemod with ars_meko

--- src/main/java/com/MajorMartian25/ars_meko/ars_meko.java
+++ src/main/java/com/MajorMartian25/ars_meko/ars_meko.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.MajorMartian25.ars_meko;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
@@ -29,11 +29,11 @@ import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(ars_meko.MODID)
+public class ars_meko
 {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "examplemod";
+    public static final String MODID = "ars_meko";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
@@ -60,7 +60,7 @@ public class ExampleMod
                 output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
             }).build());
 
-    public ExampleMod(FMLJavaModLoadingContext context)
+    public ars_meko(FMLJavaModLoadingContext context)
     {
         IEventBus modEventBus = context.getModEventBus();
 
